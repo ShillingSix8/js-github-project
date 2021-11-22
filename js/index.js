ad70049df4068d485958e54daaf372c9bfe9ae67
benjamin <benjinoble@gmail.com>
2021-11-22 16:11:14 +1100

    Workings

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+const form = document.querySelector('#github-form');
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const search = document.querySelector('#search').value
+    const searchName = search.split(' ').join('')
+
+    fetch('https://api.github.com/users/'+searchName)
+    .then((response)=> response.json())
+    .then((data)=> {
+
+        document.querySelector('#user-list').innerHTML = `<li>${data.name}</li>`;
+        document.querySelector('#avatar').innerHTML = `<img src='${data.avatar_url}'/>`;
+    })
+})
